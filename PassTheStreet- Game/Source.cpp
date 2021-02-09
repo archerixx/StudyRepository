5445abf8e751ade355d68e85c6040cdd89129cd7
Zerin Tursić <zerintursic99@gmail.com>
2021-02-09 20:54:26 +0100

    Levels added to game

--- PassTheStreet- Game/Source.cpp
+++ PassTheStreet- Game/Source.cpp
@@ -2,20 +2,121 @@
 #include <conio.h>
 #include <Windows.h>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
-const int width(30), height(16);
-int x = width / 2;
-int y = height;
-bool gameOver = false;
-const int maxC = width/7;
-bool gameWon = false;
+void gui(vector<vector<int>>, int, int, int, int, int);
+void movement(int&, int&);
+void gameLogic(vector<vector<int>>&, int, int, int, int, int, bool&, bool&);
 
-void gui(int cars[][width])  //int &carA, int& carB, int &carC, int &carD, int &carE, int&carF)
+int main()
+{
+	/*
+int carA = rand() % 20;
+int carB = rand() % 20;
+int carC = rand() % 20;
+int carD = rand() % 20;
+int carE = rand() % 20;
+int carF = rand() % 20;
+*/
+	/*
+int cars[maxH];
+for (int i = 0; i < maxH; i++)
+{
+	cars[i] = rand() % 20;
+}
+*/
+	/*
+int cars[maxH][maxW];
+for (int i = 0; i < maxH; i++)
+{
+	//int temp = maxW / (maxW/10);
+	for (int j = 0; j < maxW; j++)
+	{
+		cars[i][j] = rand() % width;
+	}
+
+}
+*/
+
+	srand(time(NULL));
+
+	int setH, setW, setMaxC, speed;
+	char difficulty;
+	char startAgain;
+	do
+	{
+		bool gameOver = false;
+		bool gameWon = false;
+		do {
+			cout << "Enter difficulty: ";
+			cin >> difficulty;
+			switch (difficulty)
+			{
+			case '1': 
+				setH = 10;
+				setW = 30;
+				setMaxC = setW / 10;
+				speed = 70;
+				break;
+			case '2':
+				setH = 16;
+				setW = 40;
+				setMaxC = setW / 8;
+				speed = 50;
+				break;
+			case '3':
+				setH = 20;
+				setW = 50;
+				setMaxC = setW / 6;
+				speed = 30;
+				break;
+			default:
+
+				break;
+			}
+		} while (difficulty > 0 && difficulty < 4);
+		const int height = setH;
+		const int width = setW;
+		const int maxC = setMaxC;
+		int x = width / 2;
+		int y = height;
+
+		vector<vector<int>> cars(height, vector<int>(width));
+		for (int i = 1; i < height; i++)
+		{
+			for (int j=0; j <maxC; j++)
+			{
+				if (i != height / 2 - 1 && i != height / 2 + 1 && i != height / 2)
+				{
+					cars[i][j] = rand() % (width-2);
+				}
+			}
+		}
+
+		while (!gameOver)
+		{
+			gui(cars, height, width, x, y, maxC);
+			movement(x,y);
+			gameLogic(cars, height, width, x, y, maxC, gameWon, gameOver);
+			Sleep(speed);
+		}
+		
+		if (gameOver == true && gameWon == true)
+			cout << "YOU WON";
+		else
+			cout << "YOU LOST";
+
+		cout << "\nWould you like to play again(y,n): ";
+		cin >> startAgain;
+	} while (startAgain != 'n');
+}
+
+void gui(vector<vector<int>> cars, int height, int width, int x, int y, int maxC)  //int &carA, int& carB, int &carC, int &carD, int &carE, int&carF)
 {
 	system("cls");
-	for (int i = 0; i <= height; i++)
+	for (int i = 0; i <=height; i++)
 	{
 		for (int j = 0; j <= width; j++)
 		{
@@ -23,7 +124,7 @@ void gui(int cars[][width])  //int &carA, int& carB, int &carC, int &carD, int &
 				cout << "o";
 			else if (i == 0 || i == height || i == height / 2 + 1 || i == height / 2 - 1)
 			{
-				
+
 				cout << "-";
 			}
 			else if (j == 0)
@@ -42,99 +143,30 @@ void gui(int cars[][width])  //int &carA, int& carB, int &carC, int &carD, int &
 				{
 					if (j == cars[i][cW])
 					{
-						for (int p= 0; p<maxC; p++){
-							if (p!=cW)
+						for (int p = 0; p < maxC; p++) {
+							if (p != cW)
 								if (cars[i][cW] == cars[i][p])
 								{
 									samePlaceCar = true;
 								}
 						}
-						if (samePlaceCar==false)
-						{ 
+						if (samePlaceCar == false)
+						{
 							cout << "B";
 							whiteSpacePrint = true;
 						}
 					}
-					
-				}
 
+				}
 				if (whiteSpacePrint == false)
 					cout << " ";
-
-				/*
-				for (int cH = maxH/2, temp=1;  temp < maxH / 2, cH < maxH; cH++, temp++)
-				{
-					if ((i == (height)/2 + temp+1) && j == cars[cH][maxH])
-					{
-						cout << "B";
-						whiteSpacePrint = true;
-					}
-
-				}
-				*/
-				/*
-				for (int cH = 0; cH < maxH/2; cH++)
-				{
-					for (int cW=0; cW< maxW; cW++)
-					{ 
-						if ((i==0+(cH+1)) && j == cars[cH][cW])
-						{ 
-							cout << "B";
-							whiteSpacePrint = true;
-						}
-					}
-				}
-				
-				for (int cH = maxH/2, temp(2); cH <= maxH ; cH++, temp++)
-				{
-					for (int cW = 0; cW < maxW; cW++)
-					{
-						if ((i == height/2+temp) && j == cars[cH][cW])
-						{
-							cout << "B";
-							whiteSpacePrint = true;
-						}
-					}
-				}
-				*/
-				/*
-				for (int cH = 0; cH <= height; cH++)
-				{
-					for (int cW = 0; cW <= width; cW++)
-					{
-						if (i == cH && j == cars[cH][cW])
-						{
-							cout << "B";
-							whiteSpacePrint = true;
-						}
-					}
-				}
-				*/
-				/*
-				if (i == height - 1 && j == carA)
-					cout << "B";
-				else if (i == height - 2 && j == carB)
-					cout << "B";
-				else if (i == height - 3 && j == carB)
-					cout << "B";
-				else if (i == height - 6 && j == carC)
-					cout << "B";
-				else if (i == height - 7 && j == carD)
-					cout << "B";
-				else if (i == height - 8 && j == carE)
-					cout << "B";
-				else if (i == height - 9 && j == carF)
-					cout << "B";
-				else
-					cout << " ";
-					*/
 			}
 		}
 		cout << endl;
 	}
 }
 
-void movement()
+void movement(int &x, int &y)
 {
 	if (_kbhit())
 	{
@@ -158,30 +190,30 @@ void movement()
 	}
 }
 
-void gameLogic(int cars[][width])   //int &carA, int &carB, int& carC, int& carD, int& carE, int& carF)
+void gameLogic(vector<vector<int>>& cars, int height, int width, int x, int y, int maxC, bool& gameWon, bool &gameOver)
 {
-	//pobjeda ili poraz
+	//win or lose
 	if (y < 0)
 	{
 		gameOver = true;
 		gameWon = true;
 	}
-	
+
 	//car hit
 	for (int cH = 1; cH < height; cH++)
 	{
 		for (int cW = 0; cW < maxC; cW++)
 		{
-			if (cH == y && cars[cH][cW]==x)
+			if (cH == y && cars[cH][cW] == x)
 				gameOver = true;
 		}
 	}
-	
+
 	//car movement (2 lanes different directions)
-	for (int cH = 0; cH < height/2; cH++)
+	for (int cH = 0; cH < height / 2; cH++)
 	{
-		for (int cW=0; cW < maxC; cW++)
-		{ 
+		for (int cW = 0; cW < maxC; cW++)
+		{
 			int rBroj = rand() % 2;
 			cars[cH][cW]++;
 			if (cars[cH][cW] == width)
@@ -195,8 +227,7 @@ void gameLogic(int cars[][width])   //int &carA, int &carB, int& carC, int& carD
 			}
 		}
 	}
-	
-	for (int cH = height/2; cH < height; cH++)
+	for (int cH = height / 2; cH < height; cH++)
 	{
 		for (int cW = 0; cW < maxC; cW++)
 		{
@@ -213,160 +244,4 @@ void gameLogic(int cars[][width])   //int &carA, int &carB, int& carC, int& carD
 			}
 		}
 	}
-
-	
-	/*
-for (int p = 0; p < height - 4; p++)
-{
-	if (cars[p] == x)
-		gameOver = true;
-}
-*/
-	/*
-for (int k = 0; k < maxH; k++)
-{
-	cars[k]++;
-	if (cars[k] == width)
-		cars[k] = 0;
-}
-*/
-	// tri reda u obe strane
-	/*
-	for (int cH = 0; cH < height / 2-4; cH++)
-	{
-		for (int cW = 0; cW < width; cW++)
-		{
-			cars[cH][cW]++;
-			if (cars[cH][cW] == width)
-				cars[cH][cW] = 0;
-		}
-	}
-
-
-	for (int cH = height/2-4; cH < height/2; cH++)
-	{
-		for (int cW = 0; cW < width; cW++)
-		{
-			cars[cH][cW]--;
-			if (cars[cH][cW] == 0)
-				cars[cH][cW] = width;
-		}
-	}
-
-	for (int cH = height / 2; cH < height/2+4; cH++)
-	{
-		for (int cW = 0; cW < width; cW++)
-		{
-			cars[cH][cW]++;
-			if (cars[cH][cW] == width)
-				cars[cH][cW] = 0;
-		}
-	}
-
-	for (int cH = height / 2+4; cH < height; cH++)
-	{
-		for (int cW = 0; cW < width; cW++)
-		{
-			cars[cH][cW]--;
-			if (cars[cH][cW] == 0)
-				cars[cH][cW] = width;
-		}
-	}
-	*/
-	/*
-	//so it can move to the right
-	carA++;
-	//for it to come back to beginning
-	if (carA == width)
-		carA = 0;
-
-	//carB
-	//so it can move to the right
-	carB++;
-	//for it to come back to beginning
-	if (carB == width)
-		carB = 0;
-
-	carC++;
-	if (carC == width)
-		carC = 0;
-
-	carD++;
-	if (carD == width)
-		carD = 0;
-
-	carE++;
-	if (carE == width)
-		carE = 0;
-
-	carF++;
-	if (carF == width)
-		carF = 0;
-		*/
-}
-
-int main()
-{
-	srand(time(NULL));
-	/*
-	int carA = rand() % 20;
-	int carB = rand() % 20;
-	int carC = rand() % 20;
-	int carD = rand() % 20;
-	int carE = rand() % 20;
-	int carF = rand() % 20;
-	*/
-	/*
-	int cars[maxH];
-	for (int i = 0; i < maxH; i++)
-	{
-		cars[i] = rand() % 20;
-	}
-	*/
-	/*
-	int cars[maxH][maxW];
-	for (int i = 0; i < maxH; i++)
-	{
-		//int temp = maxW / (maxW/10);
-		for (int j = 0; j < maxW; j++)
-		{
-			cars[i][j] = rand() % width;
-		}
-
-	}
-	*/
-
-	int cars[height][width];
-	for (int i = 1; i < height; i++)
-	{
-		for (int j=0; j <maxC; j++)
-		{
-			if (i != height / 2 - 1 && i != height / 2 + 1 && i != height / 2)
-			{
-				cars[i][j] = rand() % (width-2);
-				//if (j > 0)
-				//{
-				//		while(cars[i][j] == cars[i][j - 1])  //cars[i][j] == (cars[i][j - 1] + 1) || cars[i][j] == (cars[i][j - 1] -1) || cars[i][j] == cars[i][j - 1])
-				//		{
-				//			cars[i][j] = rand() % (width - 2);
-				//		}
-				//}
-			}
-		}
-	}
-
-	while (!gameOver)
-	{
-		gui(cars);
-		movement();
-		gameLogic(cars);
-		Sleep(0);
-	}
-		
-	if (gameOver == true && gameWon == true)
-		cout << "YOU WON";
-	else
-		cout << "YOU LOST";
-
-	cin.get();
 }
